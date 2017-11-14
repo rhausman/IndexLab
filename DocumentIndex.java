1c5ca9ae11f602cf1f5f5a7cf23e3c3ebd270c59
Naomi <naomi_cebula@horacemann.org>
2017-11-14 09:01:27 -0500

    started documentindex class
    created methods

---
+++ DocumentIndex.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class DocumentIndex here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class DocumentIndex
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class DocumentIndex
+     */
+    public DocumentIndex()
+    {
+        ArrayList<String>name = new ArrayList<String>();
+    }
+
+    public void addWord (String word, int num){    //adds num to the IndexEntry for word by calling
+                                                    //that IndexEntry's add(num) method.
+                                                    //if word is not yet in this DocumentIndex,
+                                                    //the method first creates a new IndexEntry for word
+                                                    //and inserts it into this list in alphabetical order
+                                                    //ignoring the upper and lower case.
+        
+    }
+    
+    public void allAddWords(String str, int num){
+        
+        
+    }
+    
+    private int foundOrInserted(String word){
+        
+    }
+}
