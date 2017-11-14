30b5adecfa5fa0dbb6d2166243661affa6f53751
Richard Hausman <hausman.richard@gmail.com>
2017-11-14 08:50:12 -0500

    Added methods to IndexEntry class

--- IndexEntry.java
+++ IndexEntry.java
@@ -1,7 +1,31 @@
 class IndexEntry{
+	private String word;
+	private ArrayList<Integer> numsList;
 
+	public IndexEntry(String initial)
+		{
+			word = initial.toUpperCase();
+			numsList = new ArrayList<Integer>();
+		}
 
+public int add(int n)
+{
+	if(!numsList.contains(n){numsList.add(n);}
+}
 
+public String getWord(){return word;} 
+
+public String toString()
+{
+	String temp = "";
+	for(Integer c:numsList)
+	{
+		temp += c;
+		temp += ", ";
+	}
+	temp = temp.substring(0,temp.length-2);
+	return temp;
+}
 
 
 
