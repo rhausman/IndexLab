a5c0c4c066059e5ea636db22f379a2a46efadcc0
Naomi <naomi_cebula@horacemann.org>
2017-11-15 09:00:38 -0500

    starting alternative to collections.sort
    putting this here so I don't lose it

---
+++ sort.java
@@ -0,0 +1,20 @@
+//going to call the word we are trying to insert "insertWord" for now
+
+
+
+for(int i = 0; i <arrayList.size()-1; i++){               //iterate through the arraylist
+      String currentWord = arrayList.get(i);
+      String nextWord = arrayList.get(i+1);                                                          
+      for(int j = 0; j <currentWord.size(); j ++){              //how to turn string into arraylist so that I can scan them?
+          char currentWordLetter = currentWord.charAt(j);          //if this is posisble
+                                                //check first letter
+        }  
+        for(int k = 0; k <nextWord.size(); k ++){              
+          char nextWordLetter = nextWord.charAt(k);
+                                                
+        }  
+        if()
+    
+    
+                                                
+}
