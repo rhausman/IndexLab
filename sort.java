e19eb79264145b3f2145d1d2f81ec7a722ba0a4c
Naomi <naomi_cebula@horacemann.org>
2017-11-16 08:36:59 -0500

    stuff on stack overflow

--- sort.java
+++ sort.java
@@ -1,22 +1,12 @@
 //going to call the word we are trying to insert "insertWord" for now
 
-for(int h = 0; h < insertWord.length(); h ++){
-    char insertWordLetter = insertWord.charAt(h);
-}
 
-for(int i = 0; i <arrayList.size()-1; i++){               //iterate through the arraylist
-      String currentWord = arrayList.get(i);
-      String nextWord = arrayList.get(i+1);                                                          
-      for(int j = 0; j <currentWord.size(); j ++){              //how to turn string into arraylist so that I can scan them?
-          char currentWordLetter = currentWord.charAt(j);          //if this is posisble
-                                                //check first letter
-        }  
-        for(int k = 0; k <nextWord.size(); k ++){              
-          char nextWordLetter = nextWord.charAt(k);
-                                                
-        }  
-        if()
-    
-    
-                                                
+
+boolean isSorted = true;
+for(int i = 0; i < list.size() - 1; i++) {
+   // current String is > than the next one (if there are equal list is still sorted)
+   if(list.get(i).compareToIgnoreCase(list.get(i + 1)) > 0) { 
+       isSorted = false;
+       break;
+   }
 }
