73556dd9f1a5a22be3ca625a56d3efbe2acf80d2
Richard Hausman <hausman.richard@gmail.com>
2017-11-16 09:08:28 -0500

    Finished!
    Removed dependancy on java.util.Collections

--- DocumentIndex.java
+++ DocumentIndex.java
@@ -1,15 +1,14 @@
 
 /**
  * Write a description of class DocumentIndex here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
 import java.util.*;
 public class DocumentIndex extends ArrayList<IndexEntry>
 {
-    // instance variables - replace the example below with your own
-    private int x;
+
 
     /**
      * Constructor for objects of class DocumentIndex
@@ -22,11 +21,11 @@ public class DocumentIndex extends ArrayList<IndexEntry>
 
     public DocumentIndex(int c)
     {
-	super(c);
+	     super(c);
     }
 
-    public void addWord (String word, int num){    
-	 //NOTE: consider helper method to find loc or desired loc for alphabetical organizing 
+    public void addWord (String word, int num){
+	 //NOTE: consider helper method to find loc or desired loc for alphabetical organizing
 	    //adds num to the IndexEntry for word by calling
                                                     //that IndexEntry's add(num) method.
                                                     //if word is not yet in this DocumentIndex,
@@ -34,6 +33,7 @@ public class DocumentIndex extends ArrayList<IndexEntry>
                                                     //and inserts it into this list in alphabetical order
                                                     //ignoring the upper and lower case.
 
+/*
 		ArrayList<String> sorted = new ArrayList<String>(1); //start with one capacity then go from there
 		for(IndexEntry e : this)
 		{
@@ -55,14 +55,15 @@ public class DocumentIndex extends ArrayList<IndexEntry>
 			int loc = sorted.indexOf(word.toUpperCase());//Find the location of the word we are adding
 			//System.out.println("INDEX AFTER ADDED: " + loc);
 			this.add(loc, newEntry);//we add the corresponding IndexEntry to that location
-			
-			searchResult = loc;	
-		}
 
+			searchResult = loc;
+		}
+*/
+    int searchResult = foundOrInserted(word);
 		this.get(searchResult).add(num); //whether or not the element existed before that if statement, it does now
 		//so we can add to it the num
     }
-    
+
     public void addAllWords(String str, int num){
        //String[] punctuation = {"." , "," };
        //for(int p = 0; p< punctuation.length; p++) str = str.replaceAll(punctuation[p],""); //removes all punctuation listed
@@ -72,10 +73,28 @@ public class DocumentIndex extends ArrayList<IndexEntry>
 	if(!( w.length() == 0 || space.equals(w.charAt(0)) ) ) this.addWord(w,num); //if the length is not 0 and the first char is not whitespace, add it!
        } //end of foreach loop
     }
-    
+
     private int foundOrInserted(String word){
-    return 8;    
-    }
+      int i;
+      for(i = 0; i < this.size(); i++) {
+         // current String is > than the next one (if there are equal list is still sorted)
+          int comparison = word.compareToIgnoreCase(this.get(i).getWord() );
+
+         if(comparison == 0) {
+             return i;
+
+         }
+         else if(comparison < 0){
+         		this.add(i,new IndexEntry(word));
+         		return i;
+         	}
+
+
+      }//for loop
+      //the below code should only run if the list is empty
+      this.add(i,new IndexEntry(word));
+      return i;
+    }//foundOrInserted
 
 
 }
